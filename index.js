660145d4ae1bca9dbc8b83254370ad14dd71b963
Fernando Lobos <lobosfernandoexequiel@gmail.com>
2021-06-25 11:48:53 -0300

    jsvsc

---
+++ index.js
@@ -0,0 +1,21 @@
+function calcular(operacion) {
+
+    var resultado = 0;
+    if (document.getElementById(operacion)=="Sumar") {
+        resultado = document.getElementById("num1")+document.getElementById("num2").value;
+        
+        alert("resultado"+resultado)
+    }
+    
+}
+
+function mostrar() {
+
+    var num2 = document.getElementById("num2").value;
+    var num1 = document.getElementById("num1").value;
+    var num3 = document.calc.
+
+
+    alert(num3);
+    
+}
