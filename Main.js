7d1367f4ce0e4457830f3291aa7c35caf917944c
Fernando Lobos <lobosfernandoexequiel@gmail.com>
2021-06-30 01:29:52 -0300

    al fin anda todo

--- Main.js
+++ Main.js
@@ -1,9 +1,18 @@
 function calcular() {
-
     var num1 = document.getElementById("num1").value;
     var num2 = document.getElementById("num2").value;
     var operacion = document.getElementById("operacion").value;
-    var result = eval(num1+operacion+num2)
+    var result = eval(num1 + operacion + num2)
     alert(result)
 }
+
+function jubil() {
+    if (forms.jubi[0].checked) {
+        forms.edad.disabled = false;
+    } else {
+        forms.edad.disabled = true;
+    }
+}
+
+
 
