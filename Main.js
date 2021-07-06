736ea41833905760b1c883113ac9e788e323afed
Fernando Lobos <lobosfernandoexequiel@gmail.com>
2021-07-06 12:36:44 -0300

    ad

--- Main.js
+++ Main.js
@@ -7,12 +7,28 @@ function calcular() {
 }
 
 function jubil() {
-    if (forms.jubi[0].checked) {
+    if (forms.jubi[0].checked) { // document.getElementById("siv") ahora funciona de las 2 maneras
         forms.edad.disabled = false;
     } else {
         forms.edad.disabled = true;
     }
 }
+
+
+function valideKey(evt){
+			
+    // code is the decimal ASCII representation of the pressed key.
+    var code = (evt.which) ? evt.which : evt.keyCode;
+    
+    if(code==8) { // backspace.
+      return true;
+    } else if(code>=48 && code<=57) { // is a number.
+      return true;
+    } else{ // other keys.
+      return false;
+    }
+}
+
 
 
 
